a5a0554cfefb2d92aa580eda74114578f2be8d73
James Cool <jeffreysbrother@gmail.com>
2015-10-11 12:41:30 -0700

    updated Gruntfile.js and ran grunt to compress images

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,33 @@ module.exports = function(grunt) {
   //project configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'js/script.js',
-        dest: 'script.min.js'
+
+    imagemin: {
+        jpg: {
+        options: {
+          optimizationLevel: 5,
+          progressive: true
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'img/',
+            src: ['**/*.jpg'],
+            // Could also match cwd. i.e. project-directory/img/
+            dest: 'img/compressed/',
+            ext: '.jpg'
+          }
+        ]
+        }
       }
-    }
 
   });
 
   //load the plugin that provides the 'uglify' task
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
   //default task(s)
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['imagemin']);
 };
